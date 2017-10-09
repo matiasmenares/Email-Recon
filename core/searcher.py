05d0bae27c1323fd66fe19722f2cc90dbd1b15c4
matiasmenares <dejaaymts@gmail.com>
2017-10-09 15:33:43 -0300

    Refact: Core

---
+++ core/searcher.py
@@ -0,0 +1,37 @@
+import socket
+import smtplib
+import dns.resolver
+
+class Searcher:
+
+	def __init__(self, email):
+		self.email = email
+		self.domail = self.get_domail()
+		self.mx = self.get_mx_exchange()
+
+	def cut(self,data,delimiter):
+		return data.split(delimiter)
+
+	def get_domain(self):
+		domain = self.cut(self.email,"@")
+		return str(domain[1])
+
+	def get_mx_exchange(self):
+		answers = dns.resolver.query(self.domail, 'MX')
+		for rdata in answers:
+			mxx = rdata.exchange		
+		return str(mxx)
+
+	def searcher(self):
+		host = socket.gethostname()
+		server = smtplib.SMTP()
+		server.set_debuglevel(0)
+		server.connect(self.mx)
+		server.helo(self.domail)
+		server.mail(self.email)
+		code, message = server.rcpt(str(self.email))
+		server.quit()
+		if code == 250:
+			print('Verdadero')
+		else:
+			print('Falso')
--- email-recon.py
+++ email-recon.py
@@ -1,33 +1,25 @@
-import socket
-import smtplib
-import dns.resolver
+#!/usr/bin/python
+#
+# Author(s): Matias Menares (Gh0st)
+# Email Recon
+
+from core.searcher import Searcher
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", help="Email")
-
 params = parser.parse_args()
 
-email = params.m
-domain = email.split("@")
+def main(params):
+	if params.m:
+		search.searcher()
+	else:
+		print("Type -h for help.")
 
-answers = dns.resolver.query(domain[1], 'MX')
-for rdata in answers:
-	mxx = rdata.exchange
+if __name__ == '__main__':
+	search = Searcher(params.m)
 
-host = socket.gethostname()
-# SMTP lib setup (use debug level for full output)
-server = smtplib.SMTP()
-server.set_debuglevel(0)
-# SMTP Conversation
-server.connect(str(mxx))
-server.helo(str(domain[1]))
-server.mail(email)
-code, message = server.rcpt(str(email))
-server.quit()
-
-# Assume 250 as Success
-if code == 250:
-	print('Verdadero')
-else:
-	print('Falso')
+try:
+    main(params)
+except (KeyboardInterrupt, EOFError):
+    log.info('Exiting.')
